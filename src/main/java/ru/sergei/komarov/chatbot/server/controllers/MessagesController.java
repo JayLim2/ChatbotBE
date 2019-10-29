0fd929c2a7b998f65170e883a1e2f435139d2273
JayLim <ddx180874@gmail.com>
2019-10-29 23:17:12 +0400

    saving controller refactored, added "save all" rest

--- src/main/java/ru/sergei/komarov/chatbot/server/controllers/MessagesController.java
+++ src/main/java/ru/sergei/komarov/chatbot/server/controllers/MessagesController.java
@@ -1,5 +1,7 @@
 package ru.sergei.komarov.chatbot.server.controllers;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +10,14 @@ import ru.sergei.komarov.chatbot.server.models.Message;
 import ru.sergei.komarov.chatbot.server.services.MessagesService;
 import ru.sergei.komarov.chatbot.server.services.UsersService;
 import ru.sergei.komarov.chatbot.server.utils.Converter;
+import ru.sergei.komarov.chatbot.server.utils.GsonConverter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -45,15 +50,46 @@ public class MessagesController {
     @ResponseBody
     public JsonPrimitive saveMessage(@RequestBody JsonObject jsonMessage) {
         try {
-            Message message = new Message();
-            message.setMessage(jsonMessage.get("message").getAsString());
-            message.setDate(LocalDateTime.parse(jsonMessage.get("date").getAsString(), DateTimeFormatter.ISO_DATE_TIME));
-            message.setUser(usersService.getByLogin(jsonMessage.get("userId").getAsString()));
+            Message message = parseMessageFromJson(jsonMessage);
             message = messagesService.save(message);
             return new JsonPrimitive(message.getId());
         } catch (Exception e) {
             e.printStackTrace();
             return new JsonPrimitive("FAIL");
         }
+    }
+
+    @PostMapping("/save/all")
+    @ResponseBody
+    public JsonElement saveMessages(@RequestBody JsonArray jsonMessages) {
+        List<Integer> newMessagesIds = new ArrayList<>(jsonMessages.size());
+        for (JsonElement jsonMessageRaw : jsonMessages) {
+            try {
+                JsonObject jsonMessage = jsonMessageRaw.getAsJsonObject();
+                Message message = parseMessageFromJson(jsonMessage);
+                message = messagesService.save(message);
+                newMessagesIds.add(message.getId());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        JsonElement response;
+        if(jsonMessages.size() == newMessagesIds.size()) {
+            response = GsonConverter.collectionToJsonArray(newMessagesIds);
+        } else if(newMessagesIds.size() > 0) {
+            response = new JsonPrimitive("PARTLY_FAIL");
+        } else {
+            response = new JsonPrimitive("FAIL");
+        }
+        return response;
+    }
+
+    private Message parseMessageFromJson(JsonObject jsonMessage) {
+        Message message = new Message();
+        message.setMessage(jsonMessage.get("message").getAsString());
+        message.setDate(LocalDateTime.parse(jsonMessage.get("date").getAsString(), DateTimeFormatter.ISO_DATE_TIME));
+        message.setUser(usersService.getByLogin(jsonMessage.get("userId").getAsString()));
+        return message;
     }
 }
